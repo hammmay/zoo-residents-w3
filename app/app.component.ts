b410f671bd4cb5d116d9483e65cf3fddcc62892e
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-16 15:46:05 -0700

    Add capabilities to publish page with  component files and declaration, root module, entry-point file and system configuration file

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>My Recipe Box</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
