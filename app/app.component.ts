f3263f6889628ec0217cde94b92186a8255ca79d
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-18 12:12:01 -0700

    Add dynamic copy to homepage

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,10 +5,17 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Life Is A Zoo</h1>
+    <h3>{{tagline}}</h3>
+    <ul>
+      <li>{{firstResident.name}}</li>
+    </ul>
   </div>
   `
 })
 
 export class AppComponent {
-
+  tagline: string = 'Add A New Resident OR Make Edits To An Existing Resident';
+  firstResident = {
+    name: "Francis"
+  }
 }
