14bd28828465b1d88cb48918060fd4b4774e4539
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-18 13:51:00 -0700

    Add property binding for age attribute

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
     <h1>Life Is A Zoo</h1>
     <h3>{{tagline}}</h3>
     <ul>
-      <li *ngFor="let currentResident of residents">{{currentResident.name}}   <button (click)="editResident()">Make Some Changes</button></li>
+      <li [class]="ageColor(currentResident)" *ngFor="let currentResident of residents">{{currentResident.name}}   <button (click)="editResident()">Make Some Changes</button></li>
     </ul>
   </div>
   `
@@ -16,16 +16,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   tagline: string = 'Add A New Resident OR Make Edits To An Existing Resident';
   residents: Resident[] = [
-    new Resident('Francis'),
-    new Resident('George'),
-    new Resident('Tom')
+    new Resident('Francis', 5),
+    new Resident('George', 4),
+    new Resident('Tom', 1)
   ];
 
   editResident() {
     alert("Click is working");
   }
+
+  ageColor(currentResident){
+    if (currentResident.age >= 2){
+      return "bg-danger";
+    } else {
+      return "bg-info";
+    }
+  }
+
 }
 
 export class Resident {
-  constructor(public name: string) { }
+  constructor(public name: string, public age: number) { }
 }
