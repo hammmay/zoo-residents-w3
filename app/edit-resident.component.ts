6f981f232e31b6fe51bed1b9dd4a23c8fcd71c72
Laura Hamilton <laurahamilton9@gmail.com>
2017-08-20 13:51:59 -0700

    Update edit functionality to reflect limited fields

--- app/edit-resident.component.ts
+++ app/edit-resident.component.ts
@@ -14,26 +14,8 @@ import { Resident } from './resident.model';
          <label>Enter Resident's Age In Years:</label>
          <input [(ngModel)]="childSelectedResident.age">
          <br>
-         <label>Enter Resident's Species:</label>
-         <input [(ngModel)]="childSelectedResident.species">
-         <br>
-         <label>Enter Resident's Diet:</label>
-         <input [(ngModel)]="childSelectedResident.diet">
-         <br>
-         <label>Enter Resident's Location:</label>
-         <input [(ngModel)]="childSelectedResident.location">
-         <br>
          <label>Enter Number of Caretakers Needed:</label>
          <input [(ngModel)]="childSelectedResident.caretakers">
-         <br>
-         <label>Enter Resident's Sex:</label>
-         <input [(ngModel)]="childSelectedResident.sex">
-         <br>
-         <label>Enter Resident's Likes:</label>
-         <input [(ngModel)]="childSelectedResident.likes">
-         <br>
-         <label>Enter Resident's Dislikes:</label>
-         <input [(ngModel)]="childSelectedResident.dislikes">
          <br>
          <button (click)="doneButtonClicked()">Done</button>
        </div>
